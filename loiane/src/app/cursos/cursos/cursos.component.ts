b62b8f302f09acda9b06e709c74c1258798dce68
Rodrigo Costa <rfcosta85@gmail.com>
2022-01-25 15:09:44 -0300

    Aula 06 template

--- loiane/src/app/cursos/cursos/cursos.component.ts
+++ loiane/src/app/cursos/cursos/cursos.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal: string; //Criamos a variável que irá receber a Url do nosso curso e tipamos ela como string
+
+  cursos: string[] = ['Java', 'Ext JS', 'Angular'];
+  
+  constructor() { 
+    this.nomePortal = "http://loiane.training"; //Aqui é código JS dentro do componente. Estamos referenciando o nomePortal através do this
+  }
 
   ngOnInit(): void {
   }
