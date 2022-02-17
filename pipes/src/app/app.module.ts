4fd6b63c2977d2b39137c551422d51312f4e9a62
Rodrigo Costa <rfcosta85@gmail.com>
2022-02-17 10:04:05 -0300

    Diversas aulas

---
+++ pipes/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
+import { CamelCasePipe } from './camel-case.pipe';
+import { FiltroArrayPipe } from './filtro-array.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ExemplosPipesComponent,
+    CamelCasePipe,
+    FiltroArrayPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    // {
+    //   provide: LOCALE_ID,
+    //   useValue: 'pt-BR',
+    // }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
